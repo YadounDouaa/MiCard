0cc2e21686c5c97cef4b749e1e618e598f7f1d33
YadounDouaa <57761759+YadounDouaa@users.noreply.github.com>
2020-10-07 22:58:30 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[500],
+        backgroundColor: Colors.blueGrey[600],
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
